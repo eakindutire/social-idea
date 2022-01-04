75cf7c6dc1232fee7a856d41b99f66fb799932c7
eakindutire <emmanuelakindutire@gmail.com>
2022-01-04 17:20:41 +0000

    adding image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/profilePicture.JPG';
+
+
+function Header() {
+  return (
+    <header>
+     <h1> Emmanuel's Social Media</h1> 
+    </header>
+  );
+}
+function AdditionHeader() {
+  return (
+    <section>
+      Software Engineer
+    </section>
+  )
+}
+
+function WelcomeUser(props) {
+  return (
+    <section>
+      <p>Welcome </p>
+      </section>
+
+  );
+}
+
+function ProfilePicture() {
+   // Import results is the URL of your image
+  return <img src={logo} width="500" height="500" />
+}
+
+function Body() {
+  return (
+
+    <section>
+      <p> Body </p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <section>
+    <p> bottom</p>
+    </section>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AdditionHeader />
+      <WelcomeUser />
+      <ProfilePicture />
+      <Body />
+      <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
